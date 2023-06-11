2faa2e39ea77e3cab31f1ecfaab3c82dbab9837c
Alecay <83975131+Alecay@users.noreply.github.com>
2023-06-11 13:45:08 -0500

    Added Hex Mechanics to project

--- First Project/Assets/Scripts/Sample.cs
+++ First Project/Assets/Scripts/Sample.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Sample : MonoBehaviour
 {
+    ComputeBuffer buffer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        buffer = ComputeHelper.CreateAndSetBuffer<float>(256, null, "name", 0);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnDisable()
+    {
+        ComputeHelper.Release(buffer);
     }
 }
